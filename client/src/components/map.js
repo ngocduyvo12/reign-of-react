3898bb1f7a08198746c0ea8a9f631e3d23aa5081
Ngoc Vo <ngocduyvo12@yahoo.com>
2019-11-07 15:13:24 -0700

    added note on map.js

--- client/src/components/map.js
+++ client/src/components/map.js
@@ -8,9 +8,13 @@ function hellerbenia () {
 
 function Map() {
     return (
+        //left half of the map
         <map name="game-map">
             <area shape="poly" coords="277,395,237,434,211,559,278,624,315,556,444,480, 413, 405" onClick={hellerbenia} />
         </map>
+
+        //right half of the map
+
     )
 }
 
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Draggable, { DraggableCore } from 'react-draggable'; // Both at the same time
 import Map from "../components/map"
 import "../styles/map.css";
+import "../styles/player-stat.css"
 
 class Home extends Component {
     state = {
@@ -12,16 +13,43 @@ class Home extends Component {
         const dragHandlers = { onStart: this.onStart, onStop: this.onStop };
         return (
             <div className="container-fluid">
+                <div className="row">
+                    <div className="col col-lg-8">
 
-            <div id="map-box">
-                <Draggable {...dragHandlers}>
-                    <div id="text-box" >
-                    <img src="./img/map/map.png" id="map" alt="map" useMap="#game-map" />
-                    
+                        {/* Map div */}
+                        <div id="map-box">
+                            <Draggable {...dragHandlers}>
+                                <div id="text-box" >
+                                    <img src="./img/map/map.png" id="map" alt="map" useMap="#game-map"/>
+                                </div>
+                            </Draggable>
+                            <Map />
+                        </div>
                     </div>
-                </Draggable>
-                <Map />
-            </div>
+
+                    <div className="col col-lg-4">
+                    {/* player stats div */}
+                        <div className="row">
+                            <div className="col">
+                                <div id="player-stat-box">
+                                    <div className="row align-items-center">
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    {/* consumable items div */}
+                    <div className="row">
+                            <div className="col">
+                                <div id="player-consumable-box">
+                                    {/* logic to display current consumable items */}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         )
     }
