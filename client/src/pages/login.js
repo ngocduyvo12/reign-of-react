9b572681836c9f45532baf731377f22178851245
Jake Garlick <jacobgarlick@msn.com>
2019-11-08 12:59:34 -0700

    updated login page, split login with new account creation, basic styling added

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,35 +1,52 @@
 import React, { Component } from "react";
-import Draggable, { DraggableCore } from 'react-draggable'; // 
-import Map from "../components/map"
-import "../styles/map.css";
-import "../styles/player-stat.css";
+import "../styles/login.css";
 
 class Login extends Component {
 
-    state = {
-
-    }
-
     render() {
         return (
-            <div className="jumbotron">
-                <form>
-                    <button type="submit" className="btn btn-primary">Create New Account</button>
-                    <div className="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                        <small id="emailHelp" className="form-text text-muted">Please Enter Your Email</small>
-                    </div>
-                    <div className="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
-                    </div>
-                    <div className="form-group form-check">
-                        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                    </div>
-                    <button type="submit" className="btn btn-primary">Submit</button>
-                </form>
-            </div>
+            <>
+                <div className="jumbotron">
+                    <h1>Welcome to Reign of React!</h1>
+                    <h3>Sign in or register a new account with us</h3>
+                    <form id="sign-in">
+                        <h3>Sign In</h3>
+                        <div className="form-group">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                            <small id="emailHelp" className="form-text text-muted">Please Enter Your Email</small>
+                        </div>
+                        <div className="form-group">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                            <small id="emailHelp" className="form-text text-muted">Please Enter Your Password</small>
+                        </div>
+                        <button type="submit" className="btn btn-lg btn-dark">Submit</button>
+                    </form>
+                    <form id="register">
+                        <h3>Register New Account</h3>
+                        <div className="form-group">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                            <small id="emailHelp" className="form-text text-muted">Please Enter A Email</small>
+                        </div>
+                        <div className="form-group">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                            <small id="emailHelp" className="form-text text-muted">Please Enter A Password</small>
+                        </div>
+                        <div className="form-group">
+                            <label for="exampleInputPassword1">Confirm Password</label>
+                            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                            <small id="emailHelp" className="form-text text-muted">Please Re-enter Your Password</small>
+                        </div>
+                        <button type="submit" className="btn btn-lg btn-dark">Submit</button>
+                    </form>
+                    <img className="front-card" src="https://www.maxplayingcards.com/en/wp-content/uploads/2013/10/KoNW_backLIMITED.png" />
+                    <img className="front-card" src="https://www.maxplayingcards.com/en/wp-content/uploads/2013/10/KoNW_backLIMITED.png" />
+                    <img className="front-card" src="https://www.maxplayingcards.com/en/wp-content/uploads/2013/10/KoNW_backLIMITED.png" />
+                </div>
+            </>
         )
 
     }
