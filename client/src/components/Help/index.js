1f5efd263164f66167df7d93ab63fc6e06f500b5
Jake Garlick <jacobgarlick@msn.com>
2019-11-08 17:31:13 -0700

    minor changes to help button, continuing to implement

--- client/src/components/Help/index.js
+++ client/src/components/Help/index.js
@@ -4,20 +4,12 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./style.css";
 
 class Help extends Component {
-    state = {
-        help: false
-    }
-
-    determineHelp = () => {
-        if (this.state.help) {
-            
-        }
-    }
+    
     
     render() {
         return (
             <>
-                <button onClick={this.determineHelp()}>Click For Help</button>
+                <button>Click For Help</button>
             </>
         )
     }
