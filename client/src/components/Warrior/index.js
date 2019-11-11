ffacf2f85efdf6ef1e401b7fad3f13ff6a85a64e
Jake Garlick <jacobgarlick@msn.com>
2019-11-11 12:04:35 -0700

    built in ability to reuse component for future classes, added second warrior class for testing, looking into logic for adding 4 random cards for start of game

---
+++ client/src/components/Warrior/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+// import Modal from "react-modal";
+// import { Link } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./style.css";
+
+class Warrior extends Component {
+
+    render () {
+        return (
+            <>
+            <p>The Warrior is a crazed killing machine, capable of facing opponents one on one and coming out ahead in battle. Blessed with incredible strength and fortitude, this class is capable of immense destruction. The Warrior is a great choice for you as you move through the combat arenas within Reign of React.</p>
+            <button className="btn-lg btn-dark">Select The Warrior</button>
+            </>
+        )
+    }
+}
+
+export default Warrior;
--- client/src/components/Welcome/index.js
+++ client/src/components/Welcome/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
-import Modal from "react-modal";
-import { Link } from 'react-router-dom';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import Modal from "react-modal";
+// import { Link } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./style.css";
 import Druid from "../Druid"
+import Warrior from "../Warrior"
 
 class Welcome extends Component {
 
@@ -11,11 +12,30 @@ class Welcome extends Component {
         class: ""
     }
 
-    
+    classInfo = () => {
+        if (this.state.class === "") {
+            return <h3>Select A Class For More Information</h3>
+        } else if (this.state.class === "Druid") {
+            return <Druid />
+        } else if (this.state.class === "Warrior") {
+            return <Warrior />
+        }
+    }
+
+    selectDruid = (event) => {
+        event.preventDefault()
+        this.setState({ class : "Druid" })
+    }
+
+    selectWarrior = (event) => {
+        event.preventDefault()
+        this.setState({ class : "Warrior" })
+    }
+
 
     render() {
         return (
-            <div class="container jumbotron">
+            <div className="container jumbotron">
                 <h1>Welcome To Reign of React</h1>
                 <img className="front-card" src="https://www.maxplayingcards.com/en/wp-content/uploads/2013/10/KoNW_backLIMITED.png" />
                 <img className="front-card" src="https://www.maxplayingcards.com/en/wp-content/uploads/2013/10/KoNW_backLIMITED.png" />
@@ -25,15 +45,16 @@ class Welcome extends Component {
 
                 <p>Thank you for joining us on this adventure, to begin playing, please select from available classes below.</p>
                 <p>Once you have selected a class, you will be awarded with four random cards and the game will begin, best of luck!</p>
-                <div class="row">
-                    <div class="col-md-4 class-available">
+                <div className="row">
+                    <div className="col-md-4 class-available">
                         <p>Currently Available Classes:</p>
-                        <button class="btn-lg btn-dark btn-lg">Druid</button>
+                        <button className="btn-lg btn-dark btn-lg" onClick={this.selectDruid}>Druid</button>
+                        <button className="btn-lg btn-dark btn-lg" onClick={this.selectWarrior}>Warrior</button>
                     </div>
-                    <div class="col-md-8 class-description">
-                        <Druid />
+                    <div className="col-md-8 class-description">
+                        {this.classInfo()}
                     </div>
-                    <div class="col-md-12 random-card">
+                    <div className="col-md-12 random-card">
                         <p>THIS IS WHERE RANDOM CARDS WILL BE ASSIGNED BASED ON THE PLAYER SELECTION</p>
                     </div>
                 </div>
