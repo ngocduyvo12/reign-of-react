2674bb616ee6ee78601dd31f32625251bfc33e52
Daniel <dhuerta1408@gmail.com>
2019-11-12 09:48:19 -0700

    add key value pair to map.json to connect with characters.json, ignore mock results page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Login from "./pages/login";
 import Combat from "./components/Combat";
 // import Map from "./components/map"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import PostCombat from './components/postCombat';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route exact path="/" component={Login} />
         <Route exact path="/home" component={Home} />
         <Route exact path="/combat" component={Combat} />
+        <Route exact path="/results" component={PostCombat} />
       </Switch>
     </Router>
   )
--- client/src/components/map.js
+++ client/src/components/map.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom"
 import "../styles/map.css";
 import Draggable, { DraggableCore } from 'react-draggable'; // Both at the same time
 import ImageMapper from "react-image-mapper"
-import mapJSON from "../map.json"
+import mapJSON from "../json/map.json"
 import Help from "../components/Help"
 
 
@@ -53,7 +53,7 @@ class Map extends Component {
                     <Draggable >
                         <div id="text-box" style={{ top: "0px", left: "0px" }}>
                             <ImageMapper
-                                src="../img/map/map.png"
+                                src="./img/map/map.png"
                                 map={MAP}
                                 width={1844}
                                 onClick={area => this.getMapInfoHandler(area)}
